1718a56144b831eb4e9e26e6b87b2576c260064e
Ash-x1 <ash91p@proton.me>
2026-03-23 19:34:16 +0100

    Fix the controller before_action error and edit the show html file

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,42 @@
 class TasksController < ApplicationController
-  def show
+ before_action :set_task, only: [:show, :edit, :update]
+
+  def index
+    @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def new
     @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+
+    if @task.save 
+      redirect_to @task, notice: "Task was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :due_date)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def save
   end
 end
